2aa3764c0a912f8f2e61145e22f7df9e7882f32b
andrew hah <hahdawg@yahoo.com>
2021-02-15 13:41:51 -0600

    move reqs from req*.txt to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,10 @@ setup(
         "": ["*.yaml", "*.ini"]
     },
     install_requires=[
+        "numpy==1.19.2",
+        "pandas==1.1.5",
+        "PyYAML==5.4.1",
+        "scikit_learn==0.24.1"
     ],
     zip_safe=False
 )
