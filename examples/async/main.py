f6a61a5fd9be69674ac56eb86a3ba36095e30dfa
andrew hah <hahdawg@yahoo.com>
2021-05-21 17:10:09 -0500

    clean up

--- examples/async/main.py
+++ examples/async/main.py
@@ -9,20 +9,21 @@ async def load_chunk(path):
 
 async def load_data():
     print("In load_data")
-    paths = list("abcdefghijklmnop")
+    paths = map(str, list(range(20)))
     output = asyncio.gather(*[asyncio.create_task(load_chunk(p)) for p in paths])
     await output
-    print(output)
     return output
 
 
 def normal_fcn():
-    print("In normal_fcn")
+    return "normal-output"
 
 
 def main():
-    asyncio.run(load_data())
-    normal_fcn()
+    async_out = asyncio.run(load_data()).result()
+    normal_out = normal_fcn()
+    print(async_out)
+    print(normal_out)
 
 
 if __name__ == "__main__":
