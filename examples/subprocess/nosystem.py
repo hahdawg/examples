8511492eab0e175b361ac72807f68de814e32514
hahdawg <ahah@figure.com>
2022-07-20 08:42:11 -0500

    update

--- examples/subprocess/nosystem.py
+++ examples/subprocess/nosystem.py
@@ -1,12 +1,33 @@
+"""
+Use subprocess to run commands and capture output instead of os. Both functions should mimic
+os.system, but they capture the output as a string.
+"""
 import subprocess
-from typing import List
 
 
-def echo_hello() -> None:
-    subprocess.run(["echo", "hello"], check=True)
+def ls_run(path: str, args: str = "") -> str:
+    """
+    Use subprocess.run to do an ls command.
+    """
+    popen_args = ["ls", path]
+    if len(args) > 0:
+        popen_args.append(args)
+
+    try:
+        output = subprocess.run(popen_args, check=True, capture_output=True)
+    except subprocess.CalledProcessError as e:
+        # NOTE: We have to catch the error in order to log it. Otherwise, we just get
+        # CalledProcessError, which doesn't say what happened.
+        stderr = e.stderr.decode("utf-8")
+        raise ValueError(f"Command {popen_args} failed: {stderr}") from e
+
+    return output.stdout.decode("utf-8")
 
 
-def ls(path: str, args: str = "") -> List[str]:
+def ls_popen(path: str, args: str = "") -> str:
+    """
+    Use subprocess.popen to do an ls command.
+    """
     popen_args = ["ls", path]
     if len(args) > 0:
         popen_args.append(args)
@@ -15,6 +36,6 @@ def ls(path: str, args: str = "") -> List[str]:
         stdout, stderr = pipe.communicate()
 
     if len(stderr):
-        raise RuntimeError(f"Command {popen_args} failed: {stderr}")
+        raise ValueError(f"Command {popen_args} failed: {stderr}")
 
-    return [p for p in stdout.decode("utf-8").split("\n") if len(p) > 0]
+    return stdout.decode("utf-8")
